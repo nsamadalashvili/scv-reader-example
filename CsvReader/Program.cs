f91dad2bae38a072d145c39c0fb231a77f852f5a
Nika Samadalashvili <nika.samadalashvili@thetrainline.com>
2023-04-19 15:03:56 +0100

    init project

---
+++ CsvReader/Program.cs
@@ -0,0 +1,22 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration.Attributes;
+using System.Globalization;
+
+using var reader = new StreamReader("csv_example.csv");
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+var records = csv.GetRecordsAsync<FooRecord>();
+
+await foreach (var record in records)
+{
+    Console.WriteLine("{0}_{1}", record.OriginCode, record.SourceStation);
+}
+
+public record FooRecord
+{
+    [Name("origin_code")]
+    public string? OriginCode { get; init; }
+
+    [Name("station_1")]
+    public string? SourceStation { get; init; }
+}
